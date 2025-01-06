2afa41ccfa11a06dbc13a0a034dc503e5b2bed7d
AlissonOliveira4 <alisson.santos@picpay.com>
2025-01-06 11:33:05 -0300

    Adicionando a url do site deployado no CrossOrigin

--- src/main/java/com/valores/controller/UserController.java
+++ src/main/java/com/valores/controller/UserController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "http://127.0.0.1:5500")
+@CrossOrigin(origins = {"http://127.0.0.1:5500", "https://valores-front.onrender.com" })
 @RestController
 @RequestMapping("/valores")
 public class UserController {
